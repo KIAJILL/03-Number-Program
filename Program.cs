b90b713475139cd87ff189f7c92b3e9d448d68db
KIAJILL <162531775+KIAJILL@users.noreply.github.com>
2024-03-08 12:14:00 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _03_Number_Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me your number");
+
+            int number;
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number == 0)
+            {
+                Console.WriteLine("Your number is 0");
+            }
+            if (number <0)
+            {
+                Console.WriteLine("Your number is negative");
+            }
+            if (number > 0)
+            {
+                Console.WriteLine("Your number is positive");
+
+            }
+        }
+    }
+}
